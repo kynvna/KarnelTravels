fbaae3dc3f95d021e4cd36e8a0b7b29cac20ef82
congkhanh091 <dbuicongkhanh@gmail.com>
2024-04-22 21:03:27 +0700

    a

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b34e8ed327f5dff1eb0fe081c8643580c3172e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75775ccee95a3aedf56b2c98ce165d3f3707aed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
