c0a8dcaa0861b55c89d7c30353113ee6586c0c59
unknown <kynvna@gmail.com>
2024-04-28 07:30:47 +0700

    update Dang code's structure

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42ff7e49ccc519b903da6a605edfee65d9f1f97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784616d7152c0a4ffcbe85e8f87cd7c649a71dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
