806ab4e6c85efcbc768f18e8882125f42719e8c9
congkhanh091 <dbuicongkhanh@gmail.com>
2024-04-30 11:55:31 +0700

    merge code a Ky - 30/04/2024

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1621f0d11913d1d12204beaa16c8900a15ee5690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a41681e147d990b8612e5712a2b593818f34cc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
