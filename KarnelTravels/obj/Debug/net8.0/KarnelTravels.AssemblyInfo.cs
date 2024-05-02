45a6c0a37cb16c5a6efc2e3dff01f65ebbd2ab39
vonguyenhaidang <haidang16699@gmail.com>
2024-05-02 20:40:57 +0700

    DangVNH - Update UI 02/05/2024 Moi nguoi co the su dung UI duoc roi nhe. Moi nguoi vao Misc >> Sitemap de check lai nhe. Neu can them GUI co the nhan minh.

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c1cefa153d92c2e5ac09e028b491fa68e2767e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13bb951baaf8d3cf8bf5969667f67289d1bcbf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
