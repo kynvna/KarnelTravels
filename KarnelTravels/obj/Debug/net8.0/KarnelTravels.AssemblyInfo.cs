484177d4447cf9360848d6f55b7a7b6b90481675
vonguyenhaidang <haidang16699@gmail.com>
2024-05-04 19:25:25 +0700

    DangVNH - Merge Dan Nguyen 04/05/2024

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114858881fd1d0e2f50b87fada52b69bd725ad5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8ced70c50a979272c11b02e0ce73dc708b2db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
