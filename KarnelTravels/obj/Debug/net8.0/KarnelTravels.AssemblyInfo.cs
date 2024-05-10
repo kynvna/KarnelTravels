96646851ec97586f1ff0bd220c419a02d2434ae6
vonguyenhaidang <haidang16699@gmail.com>
2024-05-10 08:52:40 +0700

    DangVNH - Merge code anh Ky 10/05/2024

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe3338368e5dde980533ca714e52d33b8fa8cc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ef04a29960d3c3180d7616758730afcf22a537")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
