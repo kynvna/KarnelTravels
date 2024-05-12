593ee1d6840be57d90ab90a4bcc2c5b2cf30741e
vonguyenhaidang <haidang16699@gmail.com>
2024-05-12 13:53:37 +0700

    dangvnh -12052024 - Update UI cho phan User cua 1) TravellingTransportView + ViewSearch + TransportDetails 2) TravellingSight + SightDetails

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930214d096e0cefa8856b8a3bfc42659e89bc465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19162dd8b00e815686e28df97e013c6f85511c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
