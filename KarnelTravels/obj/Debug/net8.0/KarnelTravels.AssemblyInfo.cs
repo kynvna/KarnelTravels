9e1c5d969c0b33eceab129727c9449f77a516871
unknown <kynvna@gmail.com>
2024-05-15 14:57:17 +0700

    update layout for chart

--- KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
+++ KarnelTravels/obj/Debug/net8.0/KarnelTravels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c36bc3f695fd0e4efe90e6823c1fa9ac4a41e66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e7d3d222de99a8e6855ee5490a6ba9d56f5c28")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
